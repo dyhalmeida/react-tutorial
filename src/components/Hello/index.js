45cdd5fe13e249ef0ba0b89add15b67ace55d8c0
Diego Almeida <dyhalmeida@gmail.com>
2020-04-19 11:55:39 -0300

    04 - Using JSX with React.createElement

--- src/components/Hello/index.js
+++ src/components/Hello/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 const Hello = () => {
-  return (
-    <div>
-      <h1>Hello JSX</h1>
-    </div>
+  return React.createElement(
+    'div', 
+    null, 
+    React.createElement(
+      'h1', 
+      null, 
+      'Hello JSX with React.createElement'
+    )
   );
 }
 
