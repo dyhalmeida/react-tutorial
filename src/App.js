da5fc57df098b290cebe4515adc62ef44a277001
Diego Almeida <dyhalmeida@gmail.com>
2020-04-19 10:49:54 -0300

    01 - Stateless function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import Greet from './components/Greet';
+
 function App() {
   return (
     <div className="App">
+      <Greet />
     </div>
   );
 }
---
+++ src/components/Greet/index.js
@@ -0,0 +1,4 @@
+import React from 'react'
+
+const Greet = () => <h1>Say Greet</h1>
+export default Greet;
