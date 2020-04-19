a5acf023e24f442c66f77a7583eac7dd3b50882c
Diego Almeida <dyhalmeida@gmail.com>
2020-04-19 11:48:15 -0300

    02 - Statefull class component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './App.css';
 
 import Greet from './components/Greet';
+import Welcome from './components/Welcome';
 
 function App() {
   return (
     <div className="App">
       <Greet />
+      <hr/>
+      <Welcome />
     </div>
   );
 }
