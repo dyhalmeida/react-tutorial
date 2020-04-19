7b25415d2bc78323627d57cdd0b67a3f5ceeec28
Diego Almeida <dyhalmeida@gmail.com>
2020-04-19 12:03:46 -0300

    Commented explanation of JSX

--- src/components/Hello/index.js
+++ src/components/Hello/index.js
@@ -1,6 +1,19 @@
 import React from 'react';
 
 const Hello = () => {
+
+  /**
+   
+    return (
+      <div>
+        <h1>Hello JSX</h1>
+      </div>
+    );
+
+    O código acim com JSX é o mesmo que o código abaixo
+    utlizando o React.createElement
+    
+   */
   return React.createElement(
     'div', 
     null, 
