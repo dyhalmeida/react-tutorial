dcd12295d873bea28746f64745260b02feaf0734
Diego Almeida <dyhalmeida@gmail.com>
2020-04-20 21:39:33 -0300

    11 - Event binding

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 
-import EventHandling from './components/EventHandling';
+import Welcome from './components/Welcome';
 
 function App() {
   return (
     <div className="App">
-      <EventHandling/>
+      <Welcome/>
     </div>
   );
 }
--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -3,29 +3,51 @@ import React, { Component } from 'react';
 class Welcome extends Component {
 
   /**
-   * O state do componente é similar as props, 
-   * mas é privado e totalmente controlado pelo componente.
+   * 4 Maneiras de event binding no React
+   * 01 - { this.handleClick.bind(this) }
+   * 02 - { () => this.handleClick() }
+   * 03 - no construtor: this.handleClick.bind(this)
+   * 04 - Propriedade como arrow function
    */
 
   constructor(props) {
     super(props);
     this.state = {
-      message: 'Click the button below to subscribe',
+      message: 'Hello',
     }
+
+    this.handleClick2 = this.handleClick2.bind(this);
   }
 
   handleClick() {
     this.setState({
-      message: 'Subscription successful'
+      message: 'Good Bye'
+    });
+  }
+
+  handleClick2() {
+    this.setState({
+      message: 'Good Bye'
+    });
+  }
+
+  handleClick3 = () => {
+    this.setState({
+      message: 'Good Bye'
     });
   }
 
   render() {
     return (
       <div>
-        <h2>Welcome { this.props.firstname } { this.props.lastname }</h2>
-        <p>{this.state.message}</p>
-        <button onClick={ () => this.handleClick() }>Subscribe</button>
+        <h2>{ this.state.message }</h2>
+        <button onClick={ this.handleClick.bind(this) }>Click</button>
+        <hr/>
+        <button onClick={ () => this.handleClick() }>Click</button>
+        <hr/>
+        <button onClick={ this.handleClick2 }>Click 2</button>
+        <hr/>
+        <button onClick={ this.handleClick3 }>Click 3</button>
       </div>
     );
   }
